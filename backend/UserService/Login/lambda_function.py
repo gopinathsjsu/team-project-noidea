87890bff5eff9c92382935cfb571beabc336e176
Patrick Bustos <saintpat97@gmail.com>
2022-03-16 22:49:36 -0700

    Fixed tests and added returnResponse

--- backend/UserService/Login/lambda_function.py
+++ backend/UserService/Login/lambda_function.py
@@ -1,11 +1,33 @@
 import json
+import logging
 from User import User
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 def lambda_handler(event, context):
-    u = User(["hello", "world"], "test@gmail.com", "test")
-    return {
-        'statusCode': 200,
-        'body': {
-            'user': json.dumps(u.toJson())
+    logger.debug('[EVENT] event: {}'.format(event))
+    # Required since we want a unified input event
+    eventBody = event
+    if type(eventBody) == str:
+        eventBody = json.loads(eventBody)
+
+    if 'body' in eventBody:
+        eventBody = eventBody['body']
+    else:
+        return {
+            'statusCode': 400,
+            'body': {
+                'message': 'Invalid input'
+            }
         }
+    u = eventBody['user']
+    
+    u = User([u['fName'], u['lName']], u['email'], u['address'])
+    return returnResponse(200, {'user': json.dumps(u.toJson())})
+
+def returnResponse(statusCode, body):
+    return {
+        'statusCode': statusCode,
+        'body': body
     }
--- backend/UserService/Login/tests/test_handler.py
+++ backend/UserService/Login/tests/test_handler.py
@@ -5,13 +5,16 @@ import lambda_function
 class TestHanderCase(unittest.TestCase):
     def test_response(self):
         print("testing valid response")
-        u = lambda_function.User(["hello", "world"], "test@gmail.com", "address")
-        response = lambda_function.lambda_handler(None, None)
+        u = lambda_function.User(["hello", "world"], "test@gmail.com", "test")
+        response = lambda_function.lambda_handler({'body': {'user': u.toJson()}}, None)
         print(response)
         self.assertEqual(response['statusCode'], 200)
-        self.assertEqual(response['body'], json.dumps(u))
+        self.assertEqual(response['body']['user'], json.dumps(u.toJson()))
 
-        print("testing invalid response")
-        response = lambda_function.lambda_handler(None, None)
+        print("testing invalid input")
+        response = lambda_function.lambda_handler({}, None)
         self.assertEqual(response['statusCode'], 400)
-        self.assertEqual(response['body'], json.dumps({'errorMessage': 'Invalid input'}))
+        # Temp error outputs
+        self.assertEqual(response['body'], {'message': 'Invalid input'})
+
+        print("testing feature not implemented")
