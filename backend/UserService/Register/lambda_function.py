31cce3bdbabc81792adc3c2a72f1edc90615df2c
Patrick Bustos <saintpat97@gmail.com>
2022-04-05 22:12:20 -0700

    basic register functionality

---
+++ backend/UserService/Register/lambda_function.py
@@ -0,0 +1,62 @@
+import json
+import logging
+import boto3
+import os
+from User import User
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+def lambda_handler(event, context):
+    logger.debug('[EVENT] event: {}'.format(event))
+    # Required since we want a unified input event
+    eventBody = event
+    if type(eventBody) == str:
+        eventBody = json.loads(eventBody)
+
+    if 'body' in eventBody:
+        eventBody = eventBody['body']
+    else:
+        return returnResponse(400, {'message': 'Invalid input, no body'})
+    
+    if 'user' not in eventBody: 
+        return returnResponse(400, {'message': 'Invalid input, no user'})
+
+    user = User(eventBody['user']['userId'], eventBody['user']['name'], eventBody['user']['email'], eventBody['user']['address'], eventBody['user']['country'], eventBody['user']['roles'])
+    try:
+        uploadUser(user)
+    except ClientError as e:
+        logger.error(e.response['Error']['Message'])
+        return returnResponse(500, {'message': 'Error uploading user'})
+    
+    return returnResponse(200, {'message': 'User created',
+                                'user': user.toJson()})
+
+def uploadUser(user):
+    logger.debug('[UPLOAD] user: {}'.format(json.dumps(user.toJson())))
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    try:
+        table.put_item(Item=user.toDict())
+    except ClientError as e:
+        logger.error(e.response['Error']['Message'])
+        return returnResponse(500, {'message': 'Error uploading user'})
+    return returnResponse(200, {'message': 'User created'})
+
+
+def returnResponse(statusCode, body):
+    logger.debug('[RESPONSE] statusCode: {} body: {}'.format(statusCode, body))
+    logger.debug('[RESPONSE] json.dumps(body): {}'.format(json.dumps(body)))
+    return {
+        'statusCode': statusCode,
+        'body': json.dumps(body),
+        'isBase64Encoded': False,
+        'headers': {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+            'Access-Control-Allow-Credentials': True,
+            'Content-Type': 'application/json'
+        }
+    }
