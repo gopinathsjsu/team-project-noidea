21f23eab98552997212345cc1b38555654bb530d
Patrick Bustos <saintpat97@gmail.com>
2022-04-13 14:42:56 -0700

    hotel creation basic outline finished

--- backend/HotelService/CreateHotel/lambda_function.py
+++ backend/HotelService/CreateHotel/lambda_function.py
@@ -3,6 +3,7 @@ import logging
 import boto3
 import os
 from Hotel import Hotel
+from User import User
 from botocore.exceptions import ClientError
 
 # from dotenv import load_dotenv
@@ -18,19 +19,53 @@ def lambda_handler(event, context):
     if type(eventBody) == str:
         eventBody = json.loads(eventBody)
 
-    if 'queryStringParameters' in eventBody:
-        eventBody = eventBody['queryStringParameters']
+    if 'body' in eventBody:
+        eventBody = eventBody['body']
     else:
         return returnResponse(400, {'message': 'Invalid input, no body'})
+    if type(eventBody) == str:
+        eventBody = json.loads(eventBody)
     
+    if 'hotel' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no hotel object'})
+    eventBody = eventBody['hotel']
     if 'hotelId' not in eventBody: 
         return returnResponse(400, {'message': 'Invalid input, no hotel'})
-    
+    if 'address' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no address'})
+    if 'country' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no country'})
+    if 'email' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no email'})
+    if 'name' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no name'})
+    if 'userId' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no userId. Cannot validate if admin'})
+    u = getUser(eventBody['userId'])
+    if 'Admin' not in u.roles:
+        return returnResponse(400, {'message': 'Invalid input, user is not an admin',
+                                    'status': 'error'})
     hotel = getHotel(eventBody['hotelId'])
-    return returnResponse(200, {'message': 'hotel found',
+    return returnResponse(200, {'message': 'hotel created',
                                 'status': 'success',
                                 'hotel': hotel.toDict()})
-    
+
+def getUser(userId):
+    dynamodb = boto3.resource('dynamodb', region_name=os.environ['AWS_REGION'])
+    userTable = dynamodb.Table(os.environ['TABLE_USER'])
+    try:
+        item = userTable.get_item(
+            TableName=os.environ['TABLE_USER'],
+            Key={
+                'userId': userId
+            }
+        )
+        if 'Item' not in item:
+            return returnResponse(400, {'message': 'Invalid userId, user does not exist'})
+        return User(userId, [item['Item']['firstName'], item['Item']['lastName']], item['Item']['email'], item['Item']['Address'], item['Item']['Country'], item['Item']['UserRoles'])
+    except ClientError as e:
+        return returnResponse(400, e.response['Error']['Message'])    
+
 def getHotel(hotelId):
     dynamodb = boto3.resource('dynamodb', region_name=os.environ['AWS_REGION'])
     hotelTable = dynamodb.Table(os.environ['TABLE_HOTEL'])
@@ -44,6 +79,23 @@ def getHotel(hotelId):
         if 'Item' not in item:
             return returnResponse(400, {'message': 'Invalid hotelId, hotel does not exist'})
         return Hotel(hotelId, item['Item']['name'], item['Item']['email'] , item['Item']['Address'], item['Item']['Country'])
+    except ClientError as e:
+        return returnResponse(400, e.response['Error']['Message'])
+
+def uploadHotel(hotelId, address, country, email, name):
+    dynamodb = boto3.resource('dynamodb', region_name=os.environ['AWS_REGION'])
+    hotelTable = dynamodb.Table(os.environ['TABLE_HOTEL'])
+    try:
+        hotelTable.put_item(
+            Item={
+                'hotelId': hotelId,
+                'name': name,
+                'email': email,
+                'Address': address,
+                'Country': country
+            }
+        )
+        return True
     except ClientError as e:
         return returnResponse(400, e.response['Error']['Message'])
 
