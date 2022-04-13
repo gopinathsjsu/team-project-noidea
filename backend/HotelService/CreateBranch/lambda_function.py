756345c5b3c92403b0fedb016d0a398836314291
Patrick Bustos <saintpat97@gmail.com>
2022-04-13 15:34:32 -0700

    finished branch creation

--- backend/HotelService/CreateBranch/lambda_function.py
+++ backend/HotelService/CreateBranch/lambda_function.py
@@ -3,6 +3,8 @@ import logging
 import boto3
 import os
 from Hotel import Hotel
+from Branch import Branch
+from User import User
 from botocore.exceptions import ClientError
 
 # from dotenv import load_dotenv
@@ -18,19 +20,65 @@ def lambda_handler(event, context):
     if type(eventBody) == str:
         eventBody = json.loads(eventBody)
 
-    if 'queryStringParameters' in eventBody:
-        eventBody = eventBody['queryStringParameters']
+    if 'body' in eventBody:
+        eventBody = eventBody['body']
     else:
         return returnResponse(400, {'message': 'Invalid input, no body'})
+    if type(eventBody) == str:
+        eventBody = json.loads(eventBody)
     
+    if 'hotel' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no hotel object'})
+    eventBody = eventBody['hotel']
     if 'hotelId' not in eventBody: 
         return returnResponse(400, {'message': 'Invalid input, no hotel'})
-    
+    if 'branchId' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no branch'})
+    if 'address' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no address'})
+    if 'country' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no country'})
+    if 'email' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no email'})
+    if 'name' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no name'})
+    if 'userId' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no userId. Cannot validate if admin'})
+    u = getUser(eventBody['userId'])
+    if 'Admin' not in u.role:
+        return returnResponse(400, {'message': 'Invalid input, user is not an admin',
+                                    'status': 'error'})
+    branch = getBranch(eventBody['branchId'], eventBody['hotelId'])
+    if branch is not None:
+        return returnResponse(400, {'message': 'Invalid input, hotel branch exists, please modify instead',
+                                    'status': 'error'})
     hotel = getHotel(eventBody['hotelId'])
-    return returnResponse(200, {'message': 'hotel found',
+    if hotel is None:
+        return returnResponse(400, {'message': 'Invalid input, hotel does not exist',
+                                    'status': 'error'})
+    uploadBranch(eventBody['branchId'], eventBody['hotelId'], eventBody['address'], eventBody['country'], eventBody['email'])
+    branch = getBranch(eventBody['branchId'], eventBody['hotelId'])
+    return returnResponse(200, {'message': 'hotel created',
                                 'status': 'success',
-                                'hotel': hotel.toDict()})
-    
+                                'hotel': hotel.toDict(),
+                                'branch': branch.toDict()})
+
+def getUser(userId):
+    dynamodb = boto3.resource('dynamodb', region_name=os.environ['AWS_REGION'])
+    userTable = dynamodb.Table(os.environ['TABLE_USER'])
+    try:
+        item = userTable.get_item(
+            TableName=os.environ['TABLE_USER'],
+            Key={
+                'userId': userId
+            }
+        )
+        if 'Item' not in item:
+            return returnResponse(400, {'message': 'Invalid userId, user does not exist'})
+        return User(userId, [item['Item']['firstName'], item['Item']['lastName']], item['Item']['email'], item['Item']['Address'], item['Item']['Country'], item['Item']['UserRoles'])
+    except ClientError as e:
+        return returnResponse(400, e.response['Error']['Message'])    
+
 def getHotel(hotelId):
     dynamodb = boto3.resource('dynamodb', region_name=os.environ['AWS_REGION'])
     hotelTable = dynamodb.Table(os.environ['TABLE_HOTEL'])
@@ -42,8 +90,25 @@ def getHotel(hotelId):
             }
         )
         if 'Item' not in item:
-            return returnResponse(400, {'message': 'Invalid hotelId, hotel does not exist'})
-        return Hotel(hotelId, item['Item']['name'], item['Item']['email'] , item['Item']['Address'], item['Item']['Country'])
+            return None
+        return Hotel(hotelId, item['Item']['Name'], item['Item']['email'] , item['Item']['Address'], item['Item']['Country'])
+    except ClientError as e:
+        return returnResponse(400, e.response['Error']['Message'])
+
+def uploadHotel(hotelId, address, country, email, name):
+    dynamodb = boto3.resource('dynamodb', region_name=os.environ['AWS_REGION'])
+    hotelTable = dynamodb.Table(os.environ['TABLE_HOTEL'])
+    try:
+        hotelTable.put_item(
+            Item={
+                'hotelId': hotelId,
+                'Name': name,
+                'email': email,
+                'Address': address,
+                'Country': country
+            }
+        )
+        return True
     except ClientError as e:
         return returnResponse(400, e.response['Error']['Message'])
 
@@ -62,3 +127,37 @@ def returnResponse(statusCode, body):
             'Content-Type': 'application/json'
         }
     }
+
+def getBranch(branchId, hotelId):
+    dynamodb = boto3.resource('dynamodb', region_name=os.environ['AWS_REGION'])
+    branchTable = dynamodb.Table(os.environ['TABLE_BRANCH'])
+    try:
+        item = branchTable.get_item(
+            TableName=os.environ['TABLE_BRANCH'],
+            Key={
+                'branchId': branchId,
+                'hotelId': hotelId
+            }
+        )
+        if 'Item' not in item:
+            return None
+        return Branch(branchId, item['Item']['hotelId'], item['Item']['Address'], item['Item']['Country'], item['Item']['email'])
+    except ClientError as e:
+        return returnResponse(400, e.response['Error']['Message'])
+
+def uploadBranch(branchId, hotelId, address, country, email):
+    dynamodb = boto3.resource('dynamodb', region_name=os.environ['AWS_REGION'])
+    branchTable = dynamodb.Table(os.environ['TABLE_BRANCH'])
+    try:
+        branchTable.put_item(
+            Item={
+                'branchId': branchId,
+                'hotelId': hotelId,
+                'Address': address,
+                'Country': country,
+                'email': email
+            }
+        )
+        return True
+    except ClientError as e:
+        return returnResponse(400, e.response['Error']['Message'])
