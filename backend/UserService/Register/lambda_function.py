a8db1cf214ba92da7fcfd556b5f7b011535a2840
Patrick Bustos <saintpat97@gmail.com>
2022-04-25 15:04:56 -0700

    Added creditcard field to register

--- backend/UserService/Register/lambda_function.py
+++ backend/UserService/Register/lambda_function.py
@@ -3,6 +3,7 @@ import logging
 import boto3
 import os
 from User import User
+from CreditCard import CreditCard
 from botocore.exceptions import ClientError
 
 logger = logging.getLogger()
@@ -27,16 +28,23 @@ def lambda_handler(event, context):
     
     if 'user' not in eventBody: 
         return returnResponse(400, {'message': 'Invalid input, no user'})
+    if 'card' not in eventBody:
+        return returnResponse(400, {'message': 'Invalid input, no card'})
 
     user = User(eventBody['user']['userId'], eventBody['user']['name'], eventBody['user']['email'], eventBody['user']['address'], eventBody['user']['country'], eventBody['user']['roles'])
+    card = CreditCard(eventBody['user']['userId'], eventBody['card']['number'], eventBody['card']['cvv'], eventBody['card']['expDate'])
     try:
+        if (getUser(user) != None):
+            return returnResponse(400, {'message': 'User already exists'})
         uploadUser(user)
+        uploadCard(card)
     except ClientError as e:
         logger.error(e.response['Error']['Message'])
         return returnResponse(500, {'message': 'Error uploading user'})
     
     return returnResponse(200, {'message': 'User created',
-                                'user': user.toJson()})
+                                'user': user.toJson(),
+                                'card': card.toDict()})
 
 def uploadUser(user):
     logger.debug('[UPLOAD] user: {}'.format(json.dumps(user.toJson())))
@@ -56,8 +64,33 @@ def uploadUser(user):
             )
     except ClientError as e:
         logger.error(e.response['Error']['Message'])
-        return returnResponse(500, {'message': 'Error uploading user2'})
+        raise e
 
+def getUser(user):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    try:
+        response = table.get_item(
+            Key={
+                'userId': user.id
+            }
+        )
+    except ClientError as e:
+        logger.error(e.response['Error']['Message'])
+        raise e
+    if 'Item' in response:
+        return response['Item']
+    return None
+
+def uploadCard(card):
+    logger.debug('[UPLOAD] card: {}'.format(json.dumps(card.toDict())))
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['TABLE_CARD'])
+    try:
+        table.put_item(Item=card.toDict())
+    except ClientError as e:
+        logger.error(e.response['Error']['Message'])
+        raise e
 
 def returnResponse(statusCode, body):
     logger.debug('[RESPONSE] statusCode: {} body: {}'.format(statusCode, body))
